dfdc1ccbbd2e5e6056907617271dd2ae18dc3493
Musab Hamid <47342534+musab118@users.noreply.github.com>
2020-08-21 16:29:25 -0500

    Updates

---
+++ precache-manifest.e991b59de45aca5f3536cda5225eea6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef4d99dda420b9cf874083d782e7d9a5",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "7e12c9e1c05ae52f0c76",
+    "url": "/my-app/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "f47e35083aff56671c85",
+    "url": "/my-app/static/css/main.07068511.chunk.css"
+  },
+  {
+    "revision": "7e12c9e1c05ae52f0c76",
+    "url": "/my-app/static/js/2.65a76b83.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.65a76b83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f47e35083aff56671c85",
+    "url": "/my-app/static/js/main.bf098f33.chunk.js"
+  },
+  {
+    "revision": "a6d2b0cd40197d05160a",
+    "url": "/my-app/static/js/runtime-main.8d0a1375.js"
+  }
+]);
